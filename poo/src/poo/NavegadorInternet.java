b01b7eb802be00f0d994a69ddd03a37b51766522
Guilherme <gui.gon.sam@gmail.com>
2023-10-22 17:09:23 -0300

    Desafio POO

---
+++ poo/src/poo/NavegadorInternet.java
@@ -0,0 +1,7 @@
+package poo;
+
+public interface NavegadorInternet {
+	void exibirPagina();
+	void adicionarNovaAba();
+	void atualizarPagina();
+}
---
+++ poo/src/poo/ReprodutorMusical.java
@@ -0,0 +1,7 @@
+package poo;
+
+public interface ReprodutorMusical {
+	void tocar();
+	void pausar();
+	void selecionarMusica();
+}
